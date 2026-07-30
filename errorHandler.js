292926cbe7294a0c397121c999965c6d627917d9
jalallal544-create <jalallal544@gmail.com>
2026-07-30 12:02:43 +0600

    Create errorHandler.js

---
+++ errorHandler.js
@@ -0,0 +1,12 @@
+export default function(err,req,res,next){
+
+    console.error(err);
+
+    return res.status(500).json({
+
+        success:false,
+        message:"Internal Server Error"
+
+    });
+
+}
