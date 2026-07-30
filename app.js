68c1799f197f10c5fe397bafed01ec93085ad0f7
jalallal544-create <jalallal544@gmail.com>
2026-07-30 12:01:17 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+export default {
+    appName: process.env.APP_NAME,
+    port: process.env.PORT,
+    jwtSecret: process.env.JWT_SECRET,
+    uploadPath: process.env.UPLOAD_PATH,
+    pdfPath: process.env.PDF_OUTPUT
+};
