b035d8ff40eed700974fa8973dd8db84a0741c9f
jalallal544-create <jalallal544@gmail.com>
2026-07-30 12:56:49 +0600

    Create sample-data.sql

---
+++ database/sample-data.sql
@@ -0,0 +1,61 @@
+INSERT INTO students(
+
+student_id,
+
+full_name,
+
+program,
+
+department,
+
+cgpa,
+
+passing_year,
+
+date_of_birth,
+
+issue_date
+
+)
+
+VALUES
+
+(
+
+'19201005',
+
+'John Doe',
+
+'Bachelor of Science',
+
+'Civil Engineering',
+
+3.85,
+
+2024,
+
+'2000-08-15',
+
+'2026-07-30'
+
+),
+
+(
+
+'19201006',
+
+'Sarah Ahmed',
+
+'Bachelor of Business Administration',
+
+'Business Administration',
+
+3.72,
+
+2023,
+
+'2001-03-22',
+
+'2025-06-15'
+
+);
