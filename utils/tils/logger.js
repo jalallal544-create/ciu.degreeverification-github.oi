c99afefa0075a2feadd78d6cc0090d6214317277
jalallal544-create <jalallal544@gmail.com>
2026-07-30 12:57:46 +0600

    Create logger.js

---
+++ utils/tils/logger.js
@@ -0,0 +1,19 @@
+import winston from "winston";
+
+export default winston.createLogger({
+
+    level:"info",
+
+    transports:[
+
+        new winston.transports.Console(),
+
+        new winston.transports.File({
+
+            filename:"logs/application.log"
+
+        })
+
+    ]
+
+});
